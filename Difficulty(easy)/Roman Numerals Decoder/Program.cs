a01d355203345021a1efbcf1405776f311cb7ac7
Dawid <kuriozalnydawid@gmail.com>
2023-06-17 13:43:32 +0200

    Binary pair refactor

---
+++ Difficulty(easy)/Roman Numerals Decoder/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void RomanDecodeTests()
+    {
+        Solution("XXI");
+        // Solution("MDCLXVI"); // 1666
+        /*  TO DO!
+            
+            add more tests
+
+            try to broke it!
+        */
+    }
+
+    public static int Solution(string roman)
+    {
+        
+
+        return 0;
+    }
+
+    private static void Main(string[] args)
+    {
+        RomanDecodeTests();
+    }
+}
