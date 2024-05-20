c87c6b79fcee87d6db332f3701743fed3b11e716
Dawid Kuraś <121308225+risenovsky13@users.noreply.github.com>
2024-05-20 22:54:55 +0000

    init

---
+++ 6 kyu /Modulus 11 - Check Digit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(AddCheckDigit("036532"));
+        Console.WriteLine(AddCheckDigit("12388878"));
+        Console.WriteLine(AddCheckDigit("111111111"));
+    }
+
+    public static string AddCheckDigit(string number)
+    {
+        string checkDigit = string.Empty;
+        int[] numbers = new int[number.Length];
+
+        Regex pattern = new Regex(@"\d");
+        MatchCollection match = pattern.Matches(number);
+
+        foreach (var item in match)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("");
+
+        
+        
+        
+        
+        
+        
+
+        return number + checkDigit;
+    }
+}
